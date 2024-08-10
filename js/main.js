8d945cfb1cb80b131d2ae6811f550f64e42972e0
neb <nabraskaamarilys@gmail.com>
2024-08-10 23:02:06 +0700

    add isi content

--- js/main.js
+++ js/main.js
@@ -85,3 +85,10 @@
     
 })(jQuery);
 
+document.addEventListener("DOMContentLoaded", function() {
+    const items = document.querySelectorAll(".timeline-item");
+
+    items.forEach((item, index) => {
+        item.style.animationDelay = `${index * 0.3}s`;
+    });
+});
